c0754d5c810568319c30a5160562ad383d032110
habib <mdhabibrahman541@gmail.com>
2024-12-20 11:25:25 -0500

    Creat basic GUI without interaction

--- gui.py
+++ gui.py
@@ -1,10 +1,11 @@
 import functions
 import FreeSimpleGUI as gui
 
-lable = gui.Text("Type in a to-do ")
+lable = gui.Text("Type in a to-do ", text_color= 'white', background_color='black')
 
 input_box = gui.InputText(tooltip="Enter todo")
-add_button = gui.Button("Add")
-window = gui.Window('My to-Do App', layout=[[lable, input_box, add_button]])
+add_button = gui.Button("Add", button_color=("white", "black"))
+
+window = gui.Window('My to-Do App', background_color = '#0A97B0', layout=[[lable, input_box, add_button]])
 window.read()
 window.close()
