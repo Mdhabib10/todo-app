5401b149feb8b5f2ec3f780f267a3b5b09779e9d
habib <mdhabibrahman541@gmail.com>
2024-12-22 11:48:37 -0500

    Implemented edit button

--- gui.py
+++ gui.py
@@ -12,8 +12,9 @@ add_button = gui.Button("Add", button_color=("white", "black"))
 
 list_box = gui.Listbox(values = functions.get_todos('todos.tx'), key ='list_items', enable_events=True, size=(45,10), font=('Helvetica', 20), background_color="#CDF5FD")
 edit_button = gui.Button("Edit", button_color=("white", "black"))
-
-layout = [[lable, input_box, add_button],[list_box, edit_button]]
+complete_button = gui.Button("Complete", button_color=("white", "black"))
+Exit_button = gui.Button("Exit", button_color=("white", "black"))
+layout = [[lable],[input_box, add_button],[list_box, edit_button, complete_button],[Exit_button]]
 
 window = gui.Window('My to-Do App', background_color = '#0A97B0', layout= layout, font=('helvetica', 20))
 
@@ -25,21 +26,32 @@ while(True):
     match event:
         case "Add":
             todos = functions.get_todos('todos.tx')
-            new_todo = values['user_inputKey'] + "\n"
+            new_todo = values['user_inputKey'].capitalize() + "\n"
             todos.append(new_todo)
             functions.write_todos('todos.tx', todos)
             window['list_items'].update(values=todos)
         case "Edit":
             user_selection = values['list_items'][0]
-            new_edit = values['user_inputKey'] +"\n"
+            new_edit = values['user_inputKey'].capitalize() +"\n"
 
             todos = functions.get_todos('todos.tx')
             index = todos.index(user_selection)
             todos[index] = new_edit
             functions.write_todos('todos.tx', todos)
             window['list_items'].update(values=todos)
+        case 'Complete':
+
+            complete_todo = values['list_items'][0]
+            todos = functions.get_todos('todos.tx')
+            todos.remove(complete_todo)
+            functions.write_todos('todos.tx', todos)
+            window['list_items'].update(values=todos)
+            window['user_inputKey'].update(value='')
         case 'list_items':
             window['user_inputKey'].update(value = values['list_items'][0])
+        case 'Exit':
+            break
+
 
         case gui.WIN_CLOSED:
             break
